9085d1aec0f290e92e952f39022b6df1c263c2ea
NoresLi <lovenerverdies@163.com>
2019-07-15 11:43:25 +0800

    服务链路追踪 zipkin

---
+++ service-miya/src/main/java/com/sjyg/lzx/servicemiya/ServiceMiyaApplication.java
@@ -0,0 +1,32 @@
+package com.sjyg.lzx.servicemiya;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@RestController
+public class ServiceMiyaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceMiyaApplication.class, args);
+    }
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+
+    @RequestMapping(value = "/miya")
+    public String getMiya(){
+        return restTemplate.getForObject("http://localhost:8988/hi",String.class);
+    }
+
+}
